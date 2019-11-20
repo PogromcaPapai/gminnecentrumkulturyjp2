02693439b6e1310ef5489ffd9651cd4ce922171d
Jakub Dakowski <jakubdakowski@gmail.com>
2019-11-20 21:36:46 +0100

    no w sumie skończony back-end

--- uploads/core/views.py
+++ uploads/core/views.py
@@ -2,24 +2,34 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .models import User, Show, Reservation
-from .forms import Reserve, Register
+from .forms import Reserve, Register, NewShow
 
 
 def home(request):
     return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+###
+
 def register(request):
     if request.method == 'POST':
         form = Register(request.POST)
         if form.is_valid():
-            processed = form.cleared_data
+            processed = form.cleaned_data
+            accounts = User.objects.filter(username=processed['username'])
+            if len(accounts)!=0:
+                return render(request, 'error.html', {'error':"Istnieje już konto o takim loginie"})
             f = User(username=processed['username'],
                      password=processed['password'], email=processed['email'])
             f.save()
-            return HttpResponseRedirect('/')
+            return render(request, 'done.html')
         else:
-            new_form = Register()
-            return render(request, 'registration_page.html', {'form': new_form})
+            return render(request, 'error.html', {'error':form.errors})
     else:
         form = Register()
         return render(request, 'registration_page.html', {'form': form})
@@ -29,31 +39,52 @@ def showlist(request):
     events = Show.objects.all()
     return render(request, 'event_list.html', {'events':events})
 
-def showdetails(request, show_id):
-    show = Show.objects(id_=int(show_id))
-    if len(show)==1:
-        reserved = Reservation.objects(event=show)
+def showdetails(request, show):
+    shows = Show.objects.filter(id=int(show))
+    if len(shows)==1:
+        show = shows[0]
+        reserved = Reservation.objects.filter(event=show)
         room = [[False for i in range(10)] for j in range(10)]
         for i in reserved:
-            room[i['row']][i['column']] = True
-        return render(request, 'show.html', {'room': room, 'title': show['name'], 'desc': show['description'], 'date': show['date'], 'hour': show['hour']})
+            room[i.column][i.row] = True
+        return render(request, 'show.html', {'room': room, 'range':range(10), 'show': show})
     else:
-        return HttpResponseRedirect('/')
+        return render(request, 'error.html', {'error':"Takie wydarzenie nie istnieje"})
 
-def reservation(request, show_id, seat):
+def reservation(request, show, column, row):
     if request.method == 'POST':
+        reserved = Reservation.objects.filter(event=show, column=column, row=row)
+        if len(reserved)!=0:
+            return render(request, 'error.html', {'error':"To miejsce jest już zarezerwowane"})
         form = Reserve(request.POST)
         if form.is_valid():
             processed = form.cleaned_data
-            accounts = User.objects(
+            accounts = User.objects.filter(
                 username=processed['username'], password=processed['password'])
             if len(accounts) == 1:
-                show = Show.objects(id_=int(show_id))
-                reserve_obj = Reservation(user=accounts, event=show, seat=seat)
+                show = Show.objects.filter(id=int(show))[0]
+                reserve_obj = Reservation(user=accounts[0], event=show, column = column, row = row)
                 reserve_obj.save()
-                return HttpResponseRedirect('/')
+                return render(request, 'done.html')
             else:
-                return HttpResponseRedirect('/')
+                return render(request, 'error.html', {'error':"Niepoprawne dane logowania"})
+        else:
+            return render(request, 'error.html', {'error':form.errors})
     else:
+        reserved = Reservation.objects.filter(event=show, column=column, row=row)
+        if len(reserved)!=0:
+            return render(request, 'error.html', {'error':"To miejsce jest już zarezerwowane"})
         form = Reserve()
         return render(request, 'reservation_page.html', {'form': form})
+
+def showform(request):
+    if request.method == 'POST':
+        form = NewShow(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'done.html')
+        else:
+            return render(request, 'error.html', {'error':form.errors})
+    else:
+        form = NewShow()
+        return render(request, 'newshow.html', {'form':form})
--- uploads/urls.py
+++ uploads/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 
 from uploads.core import views
@@ -7,7 +8,10 @@ from uploads.core import views
 urlpatterns = [
     url('^$', views.home, name='home'), 
     url('register/', views.register, name='register'),
-    url('show/<int:show>/<int:place>/', views.reservation, name='registration'),
-    url('show/<int:show>/', views.showdetails, name='details'),
-    url('show/', views.showlist, name='show_shows')
+    url('newshow/', views.showform, name='new_show'),
+    url('about/', views.about, name='about'),
+    url('contact/',views.contact, name='contact'),
+    path('show/<int:show>/<int:column>/<int:row>/', views.reservation, name='reserve'),
+    path('show/<int:show>/', views.showdetails, name='details'),
+    url('show/', views.showlist, name='event_list')
 ]
